a71147ce029ec986200428b741e77c32adb50b71
Josue Diaz <jdiaz77711@gmail.com>
2019-03-10 19:22:46 +0000

    create user profile view component and add id to route parameters

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from "vue-router";
 import GMap from "./views/GMap.vue";
 import Signup from "./views/auth/Signup";
 import Login from "./views/auth/Login";
+import ViewProfile from "./views/profile/ViewProfile";
 import firebase from "firebase";
 
 Vue.use(Router);
@@ -28,6 +29,14 @@ const router = new Router({
       path: "/login",
       name: "Login",
       component: Login
+    },
+    {
+      path: "/profile/:id",
+      name: "ViewProfile",
+      component: ViewProfile,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 });
